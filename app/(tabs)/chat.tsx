1a9bda59f0a8e41bf8ad45f07b9c5849ff99b9dd
wuzhixin <zhixin.wu@xbotspace.com>
2024-08-30 18:08:58 +0800

    add antd-rn

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,4 +1,4 @@
-import { ListItem, ThemedText, ThemedView } from '@/components'
+import { Button, ListItem, ThemedText, ThemedView } from '@/components'
 import { SafeAreaView, Text, View } from 'react-native'
 import useChat from '@/hooks/useChat'
 import { useEffect } from 'react'
@@ -7,34 +7,69 @@ import { useShallow } from 'zustand/react/shallow'
 import { ResponseCode } from '@/types'
 import { FlashList } from '@shopify/flash-list'
 import dayjs from 'dayjs'
-import ChatHeader from '@/app/(tabs)/chat/ChatHeader'
 
+import { match } from 'ts-pattern'
+import { FontAwesomeIcon, LoadingIcon } from '@/components/navigation/TabBarIcon'
 const ChatPage = () => {
   const [sessions] = useChatStore(useShallow((state) => [state.sessions]))
-  const { getSessions } = useChat({ debounceWait: 300 })
+  const { getSessions, getSessionsLoading, isError } = useChat({ debounceWait: 300 })
+  const fetchSessions = async () => {
+    const response = await getSessions({ keywords: '' })
+    if (response.code === ResponseCode.success) {
+      useChatStore.setState({ sessions: response.data })
+    }
+    return response
+  }
   useEffect(() => {
-    getSessions({ keywords: '' }).then((response) => {
-      if (response.code === ResponseCode.success) {
-        useChatStore.setState({ sessions: response.data })
-      }
-    })
-  }, [getSessions])
+    fetchSessions().then()
+    console.log('首次加载中')
+  }, [])
   return (
     <ThemedView className={'h-full'}>
       <SafeAreaView className={'h-full '}>
-        <ChatHeader />
-        <FlashList
-          data={sessions}
-          keyExtractor={(item) => item.sessionSn}
-          renderItem={({ item }) => (
-            <ListItem
-              title={item.agentName}
-              desc={<Text>{dayjs(item.updateTime).format('YYYY年MM月DD日 HH:mm:ss')}</Text>}
-            />
-          )}
-          ItemSeparatorComponent={() => <View className={'h-2'} />}
-          estimatedItemSize={200}
-        />
+        {match(getSessionsLoading)
+          .with(true, () => {
+            return (
+              <View className={'flex-row items-center gap-2 h-full justify-center'}>
+                <LoadingIcon />
+                <ThemedText>加载中...</ThemedText>
+              </View>
+            )
+          })
+          .otherwise(() => null)}
+        {match(isError)
+          .with(true, () => {
+            return (
+              <View className={'h-full items-center justify-center'}>
+                <Button
+                  onPress={() => {
+                    fetchSessions().then()
+                  }}
+                  icon={<FontAwesomeIcon name="refresh" size={18} />}>
+                  重新加载
+                </Button>
+              </View>
+            )
+          })
+          .otherwise(() => {
+            if (sessions.length === 0) {
+              return null
+            }
+            return (
+              <FlashList
+                data={sessions}
+                keyExtractor={(item) => item.sessionSn}
+                renderItem={({ item }) => (
+                  <ListItem
+                    title={item.agentName}
+                    desc={<Text>{dayjs(item.updateTime).format('YYYY年MM月DD日 HH:mm:ss')}</Text>}
+                  />
+                )}
+                ItemSeparatorComponent={() => <View className={'h-2'} />}
+                estimatedItemSize={200}
+              />
+            )
+          })}
       </SafeAreaView>
     </ThemedView>
   )
